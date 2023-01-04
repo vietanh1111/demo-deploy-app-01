72174b387c23c64ecca24cc765d2e3604c3a1237
anh.nguyenviet6@gameloft.com <vietanh6>
2023-01-04 14:40:09 +0700

    - test

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
+var express = require('express');
+var app = express();
+var fs = require("fs");
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-
-let routes = require('./api/routes') //importing route
-routes(app)
-
-app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found'})
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
 })
 
-app.listen(port)
-
-console.log('RESTful API server started on: ' + port)
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
