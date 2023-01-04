a6f34aa6e7fbe5a7b5dd8847461cc80e0205c6f9
anh.nguyenviet6@gameloft.com <vietanh6>
2023-01-04 14:52:43 +0700

    - test

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+const port = process.env.PORT || 3000
+
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       console.log( data );
@@ -9,7 +11,7 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
