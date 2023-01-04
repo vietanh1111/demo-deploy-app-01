dd5dc686d8d172c43f5b4d084910523b216c3e1f
anh.nguyenviet6@gameloft.com <vietanh6>
2023-01-04 15:03:59 +0700

    - test

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ var fs = require("fs");
 
 const port = process.env.PORT || 3000
 
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       console.log( data );
@@ -11,6 +20,15 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
+app.post('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      data["user4"] = user["user4"];
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
 var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
