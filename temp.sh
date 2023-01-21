9cbc5f87037a01d4e998e87e0bc222bb038fb557
vietanh6 <anh.nguyenviet6@gameloft.com>
2023-01-21 13:51:00 +0700

    asadsa

--- temp.sh
+++ temp.sh
@@ -9,8 +9,8 @@ python --version
 git config user.name "vietanh6"
 git config user.email "anh.nguyenviet6@gameloft.com"
 
-git init
-git remote add origin https://github.com/vietanh1111/demo-deploy-app-01.git
+# git init
+# git remote add origin https://github.com/vietanh1111/demo-deploy-app-01.git
 
 echo "aaaaaa"
 git add .
