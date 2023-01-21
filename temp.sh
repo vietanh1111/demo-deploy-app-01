f26e5c8d0827cac280768b9485eff83a0990ae85
anh.nguyenviet6@gameloft.com <vietanh6>
2023-01-21 11:26:40 +0700

    test

--- temp.sh
+++ temp.sh
@@ -4,8 +4,10 @@ git --version
 python --version
 
 git init
+git remote add origin https://github.com/vietanh1111/demo-deploy-app-01.git
+
 git config user.name "vietanh6"
 git config user.email "anh.nguyenviet6@gameloft.com"
 git add .
 git commit -m "asadsa"
-git push
+git push origin
