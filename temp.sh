9d65a3cce5630e5caac16f980b59e6ec9933efcf
vietanh1111 <121867474+vietanh1111@users.noreply.github.com>
2023-01-21 17:45:46 +0700

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -9,7 +9,12 @@ git config user.email "anh.nguyenviet6@gameloft.com"
 # git init
 # git remote add origin https://github.com/vietanh1111/demo-deploy-app-01.git
 
+a="ghp_"
+b="Wik1hg27sXecx2sJJcS8XO1R9XwtFh3LdD"
+c="7e"
+d=$a$b$c
+
 echo "aaaaaa"
 git add .
 git commit -m "asadsa"
-git push https://vietanh1111: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@github.com/vietanh1111/demo-deploy-app-01.git HEAD
+git push https://vietanh1111:${d}github.com/vietanh1111/demo-deploy-app-01.git HEAD
