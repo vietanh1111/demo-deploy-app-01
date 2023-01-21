1aac50ea0325334edb895020da5836898cbb0d4d
vietanh1111 <121867474+vietanh1111@users.noreply.github.com>
2023-01-21 17:14:35 +0700

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,13 +1,10 @@
 #! /usr/bin/bash
 pwd
 git --version
-
-
-
 python --version
 
-# git config user.name "vietanh6"
-# git config user.email "anh.nguyenviet6@gameloft.com"
+git config user.name "vietanh1111"
+git config user.email "anh.nguyenviet6@gameloft.com"
 
 # git init
 # git remote add origin https://github.com/vietanh1111/demo-deploy-app-01.git
